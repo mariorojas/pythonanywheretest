e8e00c9a7ecc5bb427a0e168f7f98b089570b420
Mario Rojas <mrojas.leyva@gmail.com>
2023-11-13 14:28:06 -0600

    implement soft deletion

--- items/models.py
+++ items/models.py
@@ -3,3 +3,4 @@ from django.db import models
 
 class Item(models.Model):
     name = models.CharField(max_length=100)
+    active = models.BooleanField(default=True)
--- items/views.py
+++ items/views.py
@@ -5,7 +5,7 @@ from .models import Item
 
 def index(request):
     context = {
-        'item_list': Item.objects.all(),
+        'item_list': Item.objects.filter(active=True),
         'message': 'Hello, World!',
     }
     return render(request, 'items/index.html', context)
